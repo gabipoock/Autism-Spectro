7f3b8a3c50cf22651c72abfc5e216381bdc50f51
Gabriela de Araujo Poock <138500369+gabipoock@users.noreply.github.com>
2024-12-04 09:58:47 -0300

    Add files via upload

---
+++ dist/login.js
@@ -0,0 +1,12 @@
+anime({
+  targets: '#dust-paarticle path',
+  translateY: [10, -150],
+  direction: 'alternate',
+  loop: true,
+  delay: function(el, i, l) {
+    return i * 100;
+  },
+  endDelay: function(el, i, l) {
+    return (l - i) * 100;
+  }
+});
